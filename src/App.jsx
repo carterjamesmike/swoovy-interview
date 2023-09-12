9f5d49f6a42414779557260f7ac9843ae6b56c5f
James Carter <carterjamesmike@gmail.com>
2023-09-11 20:36:44 -0500

    Created card component with Antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import React from 'react'
+import Card from './components/Card'
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">React App</h1>
+      <Card />
     </>
   )
 }
